4fa60c15699ee69f422bae2a7c9b93e4383e7d2e
baroxFPTU <baopqse160018@fpt.edu.vn>
2022-09-09 12:02:42 +0700

    fix 500 error when deployed to Heroku

--- src/index.js
+++ src/index.js
@@ -17,30 +17,13 @@ let app
 const bootServer = () => {
   app = express()
 
-  // app.use(
-  //   cors({
-  //     origin: env.CORS_URL,
-  //     credentials: true,
-  //     optionsSuccessStatus: 200
-  //   })
-  // )
-  app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', env.CORS_URL)
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true)
-
-    // Pass to next layer of middleware
-    next()
-  })
+  app.use(
+    cors({
+      origin: env.CORS_URL,
+      credentials: true,
+      optionsSuccessStatus: 200
+    })
+  )
 
   app.use(helmet())
   app.use(express.static('public'))
